6c33f6784f9d813c373a36bb13563cb912710479
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-02-27 23:11:01 +0100

    feat(main): Game class created

--- space_invaders/main.py
+++ space_invaders/main.py
@@ -1,25 +1,36 @@
 import pygame, sys
 
+class Game:
+    def __init__(self):
+        pass
+
+    def run(self):
+        pass
+        # Update all sprites
+
 # pygame setup
-pygame.init()
-screen_width, screen_height = 600, 600
-screen = pygame.display.set_mode((screen_width, screen_height))
-clock = pygame.time.Clock()
-running = True
+if __name__ == '__main__':
+    pygame.init()
+    screen_width, screen_height = 600, 600
+    screen = pygame.display.set_mode((screen_width, screen_height))
+    clock = pygame.time.Clock()
+    running = True
+    game = Game()
 
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+    while running:
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-    # fill the screen with black
-    screen.fill((30,30,30))
+        # fill the screen with black
+        screen.fill((30,30,30))
+        game.run()
 
-    # flip() the display to put your work on screen
-    pygame.display.flip()
+        # flip() the display to put your work on screen
+        pygame.display.flip()
 
-    clock.tick(60)  # limits FPS to 60
+        clock.tick(60)  # limits FPS to 60
 
-pygame.quit()   
+    pygame.quit()   
