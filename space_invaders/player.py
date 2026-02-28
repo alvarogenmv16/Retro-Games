c9a2c141c086d5bdc5bda3736b45a6d90b169ea7
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-02-28 18:48:29 +0100

    feat(player): Added player input & constraints

--- space_invaders/player.py
+++ space_invaders/player.py
@@ -1,7 +1,26 @@
 import pygame
 
 class Player(pygame.sprite.Sprite):
-    def __init__(self, pos):
+    def __init__(self, pos, max_x):
         super().__init__()
         self.image = pygame.image.load('../Graphics/player.png').convert_alpha()
         self.rect = self.image.get_rect(midbottom = pos)
+        self.speed = 5
+        self.max_x = max_x
+
+    def get_player_input(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            self.rect.x -= self.speed
+        if keys[pygame.K_RIGHT]:
+            self.rect.x += self.speed
+
+    def constraint(self):
+        if self.rect.x <0:
+            self.rect.x = 0
+        elif self.rect.x > self.max_x - self.rect.width:
+            self.rect.x = self.max_x - self.rect.width
+
+    def update(self):
+        self.get_player_input()
+        self.constraint()
