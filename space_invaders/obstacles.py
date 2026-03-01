357b9b1abb72c4cd12444b10337df0cb48e27886
alvarogenmv16 <alvarogenmv16@gmail.com>
2026-03-01 21:35:30 +0100

    feat(main): Added first block to game window

--- space_invaders/obstacles.py
+++ space_invaders/obstacles.py
@@ -1,9 +1,17 @@
 import pygame
 
 class Block(pygame.sprite.Sprite):
-    def __init__(self, color, x, y):
+    def __init__(self, size, color, x, y):
         super().__init__()
-        self.image = pygame.Surface((10,10))
+        self.image = pygame.Surface((size, size))
         self.image.fill(color)
         self.rect = self.image.get_rect(topleft = (x, y))
 
+shape = [
+'  xxxxxxx',
+' xxxxxxxxx',
+'xxxxxxxxxxx',
+'xxxxxxxxxxx',
+'xxxxxxxxxxx',
+'xxx     xxx',
+'xx       xx']
